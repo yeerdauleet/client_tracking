c9863f179698918aef14661367a82df1f8ef6c80
yeerdauleet <81626048+yeerdauleet@users.noreply.github.com>
2025-03-05 16:12:22 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
+
+// Временные компоненты для остальных страниц
+const Prices = () => <div>Страница с ценами</div>;
+const Investors = () => <div>Страница для инвесторов</div>;
+const Presentation = () => <div>Страница с презентацией</div>;
+const Consultation = () => <div>Страница консультации</div>;
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/prices" element={<Prices />} />
+        <Route path="/investors" element={<Investors />} />
+        <Route path="/presentation" element={<Presentation />} />
+        <Route path="/consultation" element={<Consultation />} />
+      </Routes>
+    </Router>
   );
 }
 
